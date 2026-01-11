e923480d0783e529990312ecd3bb772c4c6960be
qj0r9j0vc2 <me@qj0r9j0vc2.me>
2026-01-11 11:55:38 +0900

    feat: add pkg/log module

---
+++ pkg/log/version.go
@@ -0,0 +1,10 @@
+package log
+
+// Version information for the log module.
+const (
+	// Version is the current version of the log module.
+	Version = "1.0.0"
+
+	// MinCompatibleVersion is the minimum version that is compatible with this version.
+	MinCompatibleVersion = "1.0.0"
+)
